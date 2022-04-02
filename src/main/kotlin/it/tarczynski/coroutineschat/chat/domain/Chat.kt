7141230733049d5fd8f589f55ef73ef6227a198f
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2022-04-02 02:50:45 +0200

    Don't use MutableSharedFlow

--- src/main/kotlin/it/tarczynski/coroutineschat/chat/domain/Chat.kt
+++ src/main/kotlin/it/tarczynski/coroutineschat/chat/domain/Chat.kt
@@ -1,26 +1,42 @@
 package it.tarczynski.coroutineschat.chat.domain
 
 import it.tarczynski.coroutineschat.chat.infrastructure.Message
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.onCompletion
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class Chat {
 
-    private val channels: MutableMap<String, MutableSharedFlow<Message>> = ConcurrentHashMap()
+    private val channels: MutableMap<String, Emitter> = ConcurrentHashMap()
 
     suspend fun send(
         channel: String,
         message: Message,
     ) {
-        val channelMessageFlow = channels.computeIfAbsent(channel) { MutableSharedFlow() }
-        channelMessageFlow.emit(message)
+        val channelMessageFlow = channels.computeIfAbsent(channel) { setupEmitter(channel) }
+        channelMessageFlow.channel.send(message)
     }
 
 
-    fun forChannel(
+    suspend fun forChannel(
         channel: String,
-    ): Flow<Message> = channels.computeIfAbsent(channel) { MutableSharedFlow() }
+    ): Flow<Message> =
+        coroutineScope {
+            setupEmitter(channel).channelFlow
+        }
+
+    private fun setupEmitter(channel: String) = channels.computeIfAbsent(channel) {
+        Emitter { channels.remove(channel) }
+    }
 }
+
+data class Emitter(
+    val channel: Channel<Message> = Channel(),
+    val channelFlow: Flow<Message> = channel.consumeAsFlow().onCompletion { cleanup() },
+    val cleanup: () -> Unit,
+)
--- src/main/kotlin/it/tarczynski/coroutineschat/chat/infrastructure/ChatEndpoint.kt
+++ src/main/kotlin/it/tarczynski/coroutineschat/chat/infrastructure/ChatEndpoint.kt
@@ -3,7 +3,12 @@ package it.tarczynski.coroutineschat.chat.infrastructure
 import it.tarczynski.coroutineschat.chat.domain.Chat
 import kotlinx.coroutines.flow.Flow
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/channels")
@@ -12,7 +17,7 @@ class ChatEndpoint(
 ) {
 
     @GetMapping("/{channel}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun everyone(@PathVariable channel: String): Flow<Message> = chat.forChannel(channel)
+    suspend fun everyone(@PathVariable channel: String): Flow<Message> = chat.forChannel(channel)
 
     @PostMapping("/{channel}/send")
     suspend fun sendMessage(@PathVariable channel: String, @RequestBody message: Message) {
